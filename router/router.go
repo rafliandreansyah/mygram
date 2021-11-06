2b261a408635c9eb3bb0d0dafde60941a493c6f6
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2021-11-06 10:09:11 +0700

    Create all router

---
+++ router/router.go
@@ -0,0 +1,44 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func MainRouter() *gin.Engine {
+
+	r := gin.Default()
+
+	userRouter := r.Group("/users")
+	{
+		userRouter.POST("/login")
+		userRouter.POST("/register")
+		userRouter.PUT("/")
+		userRouter.DELETE("/")
+	}
+
+	photosRouter := r.Group("/photos")
+	{
+		photosRouter.POST("/")
+		photosRouter.GET("/")
+		photosRouter.GET("/:photo_id")
+		photosRouter.PUT("/:photo_id")
+		photosRouter.DELETE("/:photo_id")
+	}
+
+	commentsRouter := r.Group("/comments")
+	{
+		commentsRouter.POST("/")
+		commentsRouter.GET("/")
+		commentsRouter.PUT("/:comment_id")
+		commentsRouter.DELETE("/:comment_id")
+	}
+
+	socialMediasRouter := r.Group("/socialmedias")
+	{
+		socialMediasRouter.POST("/")
+		socialMediasRouter.GET("/")
+		socialMediasRouter.PUT("/:socialmedia_id")
+		socialMediasRouter.DELETE("/:socialmedia_id")
+	}
+
+	return r
+
+}
