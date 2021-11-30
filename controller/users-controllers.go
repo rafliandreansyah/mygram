88ad66fb50505e9b7a41bd09a4d781a268f9bf9c
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2021-11-30 20:59:40 +0700

    Fixing error on delete user and change parameter response register and login

--- controller/users-controllers.go
+++ controller/users-controllers.go
@@ -1,12 +1,15 @@
 package controller
 
 import (
+	"MyGram/constant"
 	"MyGram/database"
 	"MyGram/helper"
 	"MyGram/model"
 	"fmt"
 	"github.com/asaskevich/govalidator"
+	"github.com/golang-jwt/jwt/v4"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	uuid "github.com/satori/go.uuid"
@@ -16,15 +19,19 @@ func Register(c *gin.Context) {
 	var err error
 	var user model.User
 	var responseRegister struct {
-		ID    uuid.UUID `json:"id"`
-		Email string 	`json:"email"`
+		ID        uuid.UUID `json:"id"`
+		Age       int       `json:"age"`
+		Email     string    `json:"email"`
+		Password  string    `json:"password"`
+		Username  string    `json:"username"`
+		CreatedAt time.Time `json:"created_at"`
 	}
-	var db  = database.GetDB()
+	var db = database.GetDB()
 
 	contentType := helper.GetContentType(c)
 
 	//Check content type
-	if contentType == helper.JSON {
+	if contentType == constant.JSON {
 		//Bind from json to struct
 		err = c.ShouldBindJSON(&user)
 		if err != nil {
@@ -68,10 +75,13 @@ func Register(c *gin.Context) {
 
 	responseRegister.ID = user.ID
 	responseRegister.Email = user.Email
+	responseRegister.Age = user.Age
+	responseRegister.Password = user.Password
+	responseRegister.Username = user.UserName
+	responseRegister.CreatedAt = user.CreatedAt
 
 	c.JSON(http.StatusCreated, gin.H{
-		"message": "user created",
-		"user":    responseRegister,
+		"Data":    responseRegister,
 	})
 
 }
@@ -81,13 +91,8 @@ func Login(c *gin.Context) {
 	var user model.User
 	var db = database.GetDB()
 
-	var loginResponse struct{
-		ID uuid.UUID `json:"id"`
-		Email string `json:"email"`
-	}
-
 	contentType := helper.GetContentType(c)
-	if contentType == helper.JSON {
+	if contentType == constant.JSON {
 		err = c.ShouldBindJSON(&user)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
@@ -95,7 +100,7 @@ func Login(c *gin.Context) {
 			})
 			return
 		}
-	}else{
+	} else {
 		err = c.ShouldBind(&user)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
@@ -110,14 +115,12 @@ func Login(c *gin.Context) {
 	// Get check email from database
 	err = db.Debug().Where(&model.User{Email: user.Email}).First(&user).Error
 	if err != nil {
-		fmt.Println("Error get user by email: ",err.Error())
+		fmt.Println("Error get user by email: ", err.Error())
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"error": "email or password invalid",
 		})
 		return
 	}
-
-
 
 	// Check password is match
 	isMatch := helper.CompareHashPassword(user.Password, password)
@@ -130,18 +133,45 @@ func Login(c *gin.Context) {
 
 	token, err := helper.GenerateToken(user.ID, user.UserName, user.Email, user.Age)
 	if err != nil {
-		 c.JSON(http.StatusBadRequest, gin.H{
-			 "message": "Error login",
-		 })
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Error login",
+		})
 		return
 	}
 
-	loginResponse.ID = user.ID
-	loginResponse.Email = user.Email
 	c.JSON(http.StatusOK, gin.H{
-		"success": "Login success",
 		"token": token,
-		"user": loginResponse,
+	})
+
+}
+
+func DeleteUser(c *gin.Context) {
+
+	var user model.User
+	db := database.GetDB()
+	userData := c.MustGet(constant.UserData).(jwt.MapClaims)
+
+	//Check user found
+	userId := uuid.Must(uuid.FromString(userData["id"].(string)))
+	rowAffected := db.First(&user, userId).RowsAffected
+	if rowAffected < 1 {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "not authenticated",
+		})
+		return
+	}
+
+	err := db.Select("Photos").Delete(&model.User{ID: userId}).Error
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "failed delete account",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "user deleted",
 	})
 
 }
